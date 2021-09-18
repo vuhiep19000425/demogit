f6290b9e789adb7b98af9b7181851d85a5bbbe88
TranMinhVu222 <tranminhvu_t64@hus.edu.vn>
2021-09-18 22:00:00 +0700

    Add Phone.java

---
+++ Phone.java
@@ -0,0 +1,22 @@
+package bai_tap_interface_abstract;
+public abstract class Phone	{
+	private String name;
+	private String phone;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getPhone() {
+		return phone;
+	}
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+	public abstract void insertPhone(String name, String phone);
+	public abstract void removePhone(String name);
+	public abstract void updatePhone(String name, String newnumber);
+	public abstract void searchPhone(String name);
+	public abstract void sort();
+}
